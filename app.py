37ca353c127258297d6d9ac0c6ad489c6d1c61e9
Meshackoo <116368127+Meshackoo@users.noreply.github.com>
2026-04-02 03:35:15 +0300

    git pull is a combination of fetch and merge

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def greet(name):
-    return f"Hello, {name}! Welcome to Git practice. by W3SCHOOL"
+    return f"Hello, {name}! Welcome to Git practice. by W3SCHOOL by meshack"
 
 if __name__ == "__main__":
     user = input("Enter your name: ")
