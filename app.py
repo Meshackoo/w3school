1b1bfcacde306afa211a0bbdcad7a70dbbfb4f07
Meshackoo <116368127+Meshackoo@users.noreply.github.com>
2026-04-02 03:24:08 +0300

    Update app.py
    practising pull request

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def greet(name):
-    return f"Hello, {name}! Welcome to Git practice."
+    return f"Hello, {name}! Welcome to Git practice. by W3SCHOOL"
 
 if __name__ == "__main__":
     user = input("Enter your name: ")
